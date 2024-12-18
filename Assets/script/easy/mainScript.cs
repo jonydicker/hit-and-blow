64b9ed0686acdc4cc674073d90ba545c82654336
jonydicker <c3116@oic.jp>
2024-12-18 14:31:49 +0900

    スクリプトの追加のみ

---
+++ Assets/script/easy/mainScript.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mainScript : MonoBehaviour
+{
+    [Header("³‰ğ‚Ìƒ{[ƒ‹"), SerializeField] public GameObject[][] correctBalls = new GameObject[4][];
+
+    private void Start()
+    {
+  
+    }
+
+}
